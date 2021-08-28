b3c015d46a7ef714dab0a6076b955ff461e1e6ec
bharghav <bharghavchinna@gmail.com>
2021-08-28 21:54:23 +0530

    latest reactjs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import Service from "./pages/Service.jsx";
-import Users from "./pages/Users.jsx";
-import Login from "./components/Login.jsx";
-
-
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/services" component={Service}/>
-          <Route path="/users" component={Users}/>
-          <Route path="/login" component={Login}/>
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
